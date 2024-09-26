2a051190bd1ba3135cf790ce87cb251654c094da
deepaung <i.deepaung@gmail.com>
2024-09-26 16:01:02 +0700

    feat: repl in main.go

--- main.go
+++ main.go
@@ -1,40 +1,32 @@
 package main
 
-func main() {
-	// client, conn := newClient()
-	// defer conn.Close()
-	//
-	// scanner := bufio.NewScanner(os.Stdin)
-	// for {
-	// 	fmt.Print("input math expression: ")
-	// 	if !scanner.Scan() {
-	// 		break
-	// 	}
-	// 	input := scanner.Text()
-	// 	if input == "" {
-	// 		break
-	// 	}
-	//
-	// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	// 	defer cancel()
-	//
-	// 	res, err := client.Calculate(ctx, &pb.Expression{Expression: input})
-	// 	if err != nil {
-	// 		fmt.Printf("error: %v\n", err)
-	// 	} else {
-	// 		fmt.Println(res.Result)
-	// 	}
-	// }
-}
+import (
+	"bufio"
+	"fmt"
+	"os"
 
-// func newClient() (pb.CalculatorClient, *grpc.ClientConn) {
-// 	opts := []grpc.DialOption{
-// 		grpc.WithTransportCredentials(insecure.NewCredentials()),
-// 	}
-//
-// 	conn, err := grpc.NewClient(constant.Address, opts...)
-// 	if err != nil {
-// 		log.Fatalf("grpc.NewClient failed: %v", err)
-// 	}
-// 	return pb.NewCalculatorClient(conn), conn
-// }
+	"github.com/DeepAung/qcal/calculator"
+)
+
+func main() {
+	calc := calculator.NewCalculator()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("input math expression: ")
+		if !scanner.Scan() {
+			break
+		}
+		input := scanner.Text()
+		if input == "" {
+			break
+		}
+
+		result, err := calc.Calculate([]byte(input))
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+		} else {
+			fmt.Println(result)
+		}
+	}
+}
